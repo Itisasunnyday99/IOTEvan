118286a250c399ca2408811238a39c4abe9087b1
Itisasunnyday99 <151415321+Itisasunnyday99@users.noreply.github.com>
2023-11-19 13:14:45 +0800

    Update lesson1_1.py

--- lesson1_1.py
+++ lesson1_1.py
@@ -1,1 +1,10 @@
-print("Hello python")
+import random as rd
+
+m = 0
+for i in range(100):
+    n = rd.randint(1, 50)
+    print(n, end=' ')
+    if n == 25:
+        m += 1
+
+print('\n 25出現了', m, '次')
